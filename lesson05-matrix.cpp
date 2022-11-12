83bb8b307d3548c5742a4434f549955d02545e83
NastyaDovgopolaia <91183611+NastyaDovgopolaia@users.noreply.github.com>
2022-11-12 12:55:43 +0300

    Add files via upload

---
+++ lesson05-matrix.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+template<typename Field, size_t M, size_t N>
+class Matrix {
+private:
+    Field Mat[M][N];
+public:
+    Matrix(Field ** Mat) {
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++)
+                this->Mat[i][j] = Mat[i][j];
+        }
+    }
+
+    void print() {
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++)
+                cout << this->Mat[i][j] << "\t";
+            cout << endl;
+        }
+    }
+    Matrix& operator+=(const Matrix& other) {
+        for (int i = 0; i < M; i++)
+            for (int j = 0; j < N; j++)
+                this->Mat[i][j] += other.Mat[i][j];
+        return *this;
+    }
+    Matrix& operator-=(const Matrix& other) {
+        for (int i = 0; i < M; i++)
+            for (int j = 0; j < N; j++)
+                this-Mat[i][j] -= other.Mat[i][j];
+        return *this;
+    }
+    Matrix operator*(int T) {
+        Matrix mult;
+        for (int i = 0; i < M; i++)
+            for (int j = 0; j < N; j++)
+                this->Mat[i][j] = T*this->Mat[i][j];
+        return *this;
+    }
+};
+
+//template<typename Field, size_t M, size_t N, size_t K>
+//Matrix<Field, M, N> operator*(const Matrix<Field, M, N>& lha, const Matrix<Field, N, K>& rha) {
+//
+//}
+
+int main() {
+
+    return 0;
+
+
+}
